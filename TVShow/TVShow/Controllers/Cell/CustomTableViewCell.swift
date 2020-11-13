75e181f6ea9c43aee9e2c5bba36ca2e35479d102
Jordi Milla Catalán <jb.90@hotmail.es>
2020-11-13 10:27:53 +0100

    Add the navigationController put another viewController i bind it.

--- TVShow/TVShow/Controllers/Cell/CustomTableViewCell.swift
+++ TVShow/TVShow/Controllers/Cell/CustomTableViewCell.swift
@@ -15,18 +15,19 @@ class CustomTableViewCell: UITableViewCell {
     @IBOutlet weak var labelCell: UILabel!
     
     var data:Movie?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
     
     func configure(movie: Movie?) {
+        
         self.data = movie
         labelCell.text = self.data?.name
         imageCell.sd_setImage(with: URL(string: self.data?.images.medium ?? ""), placeholderImage: UIImage(named: ""))
---
+++ TVShow/TVShow/Controllers/OneMovieViewController.swift
@@ -0,0 +1,20 @@
+//
+//  OneMovieViewController.swift
+//  TVShow
+//
+//  Created by Jordi Milla on 13/11/2020.
+//  Copyright © 2020 Jordi Milla Catalan. All rights reserved.
+//
+
+import UIKit
+
+class OneMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+    
+
+}
